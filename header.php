9eb91d15d740819c08da17a3c52e5ab69321b5bd
Tyler Waitt <tylerwaitt@gmail.com>
2015-03-18 18:29:42 -0400

    init

---
+++ header.php
@@ -0,0 +1,61 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+
+<html>
+<head>
+	<title>I Do The Sports</title>
+	<link rel="stylesheet" href="css.css">
+	<link href='http://fonts.googleapis.com/css?family=Droid+Sans:regular,bold' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Crimson+Text' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Cantarell:regular,italic,bold' rel='stylesheet' type='text/css'>
+	<link rel="shortcut icon" href="favicon3.ico" />
+	<script type="text/javascript" src="http://idothesports.com/flow/flowplayer-3.2.6.min.js"></script>
+	<link rel="stylesheet" href="/css/mediaboxAdvBlack21.css" type="text/css" media="screen" />
+	<script src="/js/mootools.js" type="text/javascript"></script>
+	<script src="/js/Quickie.js" type="text/javascript"></script>
+	<script src="/js/mediabox.js" type="text/javascript"></script>
+
+<?html if($page=='bio'){ ?>
+<style type="text/css">
+<!--
+body{
+background: url('images/bg-w.jpg') no-repeat;background-attachment: fixed;
+}
+-->
+</style>
+<?php } ?>
+
+
+	
+</head>
+<body>
+	
+
+	
+	
+
+	
+	
+	
+<div id="wrapper">
+
+
+<div id="top">
+<div id="top_inner">	
+	<table cellspacing="0" cellpadding="0" border="0" width="100%">
+	<tr><td>
+		<a href="index.html">home</a> |
+		<a href="reels.html">reels</a> |
+		<a href="http://idothesports.blogspot.com/">blog</a> |
+		<a href="https://drive.google.com/file/d/0B7R04svy0hlDT0NHR0dEMTZWYjg/edit">resume</a> |
+		<a href="bio.html">bio</a> |
+		<a href="contact.html">contact</a> 
+	</td>
+	</tr></table>
+	
+</div>
+</div>
+
+
+
+<div id="mid">
+<div id="mid_inner">
